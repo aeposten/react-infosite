4ad71e8b18e5366fa7f9379455c1d9157aad9742
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-20 12:56:46 -0800

    Added alt tag to image

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 function Header() {
   return (
     <header className="cat-header">
-      <img src={require("./img/icon.png")} className="cat-icon" />
+      <img src={require("./img/icon.png")} className="cat-icon" alt="the face of a cat"/>
       <h1>Meow</h1>
       <Nav />
     </header>
